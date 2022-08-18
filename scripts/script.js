8d3786d6b3ce3830a17934bf863b34067a4480ab
Axel Langerman <solomonsilversmith@gmail.com>
2022-08-18 14:43:49 -0300

    Incorporar eventos en progreso

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,33 @@
 debugger;
 
 const codigo_paises = {
-    "EUR" : "EU",
-    "ARS" : "AR",
-    "AUD" : "AU",
-    "BOB" : "BO",
-    "BRL" : "BR",
-    "CAD" : "CA",
-    "CHF" : "CH",
-    "CLP" : "CL",
-    "COP" : "CO",
-    "DKK" : "DK",
-    "GBP" : "GB",
-    "ILS" : "IL",
-    "INR" : "IN",
-    "ISK" : "IS",
-    "JPY" : "JP",
-    "KRW" : "KR",
-    "MXN" : "MX",
-    "NOK" : "NO",
-    "NZD" : "NZ",
-    "PEN" : "PE",
-    "PHP" : "PH",
-    "PYG" : "PY",
-    "RUB" : "RU",
-    "SEK" : "SE",
-    "TRY" : "TR",
-    "USD" : "US",
-    "UYU" : "UY"
+    "EU" : "EUR",
+    "AR" : "ARS",
+    "AU" : "AUD",
+    "BO" : "BOB",
+    "BR" : "BRL",
+    "CA" : "CAD",
+    "CH" : "CHF",
+    "CL" : "CLP",
+    "CO" : "COP",
+    "DK" : "DKK",
+    "GB" : "GBP",
+    "IL" : "ILS",
+    "IN" : "INR",
+    "IS" : "ISK",
+    "JP" : "JPY",
+    "KR" : "KRW",
+    "MX" : "MXN",
+    "NO" : "NOK",
+    "NZ" : "NZD",
+    "PE" : "PEN",
+    "PH" : "PHP",
+    "PY" : "PYG",
+    "RU" : "RUB",
+    "SE" : "SEK",
+    "TR" : "TRY",
+    "US" : "USD",
+    "UY" : "UYU"
 }
 
 const dropList = document.querySelectorAll("form select");
@@ -42,6 +42,7 @@ const obtenerBoton = document.querySelector("form button");
 window.onload = () => {
     imagenFrom.src = "imagenes/white-flag.png";
     imagenTo.src = "imagenes/white-flag.png";
+    obtenerTipoCambio();
 }
 
 
@@ -66,12 +67,12 @@ function obtenerTipoCambio() {
     }
 
     let url = `https://v6.exchangerate-api.com/v6/f97e5b3cb88c6e38d88a4f49/latest/${selectorInicial.value}`;
-
-    fetch(url).then(response => response.json()).then(result => {
-        let tipoCambio = result.rates[selectorFinal.value];
+    fetch(url).then(response => response.json()).then(result =>{
+        let tipoCambio = result.conversion_rates[selectorFinal.value];
         let tasaTotal = (valorMonto * tipoCambio).toFixed(2);
-        mostrar.innerText = `${valorMonto} ${selectorInicial.value} = ${tasaTotal} ${selectorFinal.value}`; 
-    }).catch(() => {
-        mostrar.innerText = "Algo fue mal.";
+        mostrar.innerText = `${valorMonto} ${selectorInicial.value} = ${tasaTotal} ${selectorFinal.value}`;
+    }).catch(() =>{
+        mostrar.innerText = "Algo salio mal";
     });
 }
+
